bbeea9bf736245f4cce1f44c72a451e5f080f0f3
mm <hahjw783@gmail.com>
2025-09-05 10:29:37 +0330

    Remove CTF folder from repository

--- CTF/home/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('',views.Home,name='home'),
-    path('scoreboard/',views.Scoreboard,name='scoreboard')
-]
--- CTF/static/js/gate.js
+++
@@ -1,7 +0,0 @@
-const passwordInput = document.getElementById('password');
-const togglePassword = document.getElementById('togglePassword');
-
-togglePassword.addEventListener('click', () => {
-    const type = passwordInput.getAttribute('type') === 'password' ? 'text' : 'password';
-    passwordInput.setAttribute('type', type);
-});
